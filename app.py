5ad751e0cd954820dcd23b90ff1358fda545c898
Sahar Abdi <102381157+saharabdi@users.noreply.github.com>
2024-10-04 12:38:31 +0330

    Update app.py
    Update

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template, redirect, url_for, request, session, f
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 import secrets
+from models import  predict  
 
 app = Flask(__name__)
+
+
 
 #app configs
 app.config['SECRET_KEY'] = secrets.token_hex(16)
@@ -29,6 +32,27 @@ def logout():
 
 @app.route('/input' , methods=['GET', 'POST'])
 def input_data():
+    if request.method == 'POST':
+        # Retrieve data from the form
+        features = {
+            'mean radius': float(request.form['mean_radius']),
+            'mean perimeter': float(request.form['mean_perimeter']),
+            'mean area': float(request.form['mean_area']),
+            'mean concavity': float(request.form['mean_concavity']),
+            'mean concave points': float(request.form['mean_concave_points']),
+            'worst radius': float(request.form['worst_radius']),
+            'worst perimeter': float(request.form['worst_perimeter']),
+            'worst area': float(request.form['worst_area']),
+            'worst concavity': float(request.form['worst_concavity']),
+            'worst concave points': float(request.form['worst_concave_points'])
+        }
+    
+        # Make a prediction using the predict function 
+        prediction = predict(features)
+        # Interpret the result
+        result = 'Malignant' if prediction == 1 else 'Benign'
+        # Redirect to the result page with the prediction
+        return render_template('result.html', prediction=result)
     return render_template('input.html')
 
 @app.route('/result')
